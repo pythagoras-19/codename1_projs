170e40dbd48d83499505a39930f24f58d4fd4c93
Matt Christiansen <39605194+pythagoras-19@users.noreply.github.com>
2023-02-19 09:52:02 -0800

    Update ISteerable.java

--- A1Proj/src/com/mycompany/myapp/ISteerable.java
+++ A1Proj/src/com/mycompany/myapp/ISteerable.java
@@ -1,11 +1,11 @@
 /**
- * 
+ * Interface for steering Movable objects.
  */
 package com.mycompany.myapp;
 
 /**
  * @author mchristiansen
- *
+ * This interface allows Movable objects to change their heading to maneuver around the GameWorld.
  */
 public interface ISteerable {
 
